77d3f3cd7778a95f7ce26b80a400c7c0b72e7833
thehappyman <1246505492@qq.com>
2014-07-29 16:09:41 +0800

    overthis

--- js/2.js
+++ js/2.js
@@ -5,7 +5,17 @@ foo.prototype.sayHello = function(){
     alert(this.myName);
 };
 foo.prototype.bar = function(){
-    setTimeout(this.sayHello, 1000);
+    setTimeout(this.sayHello(), 1000);
+//    setTimeout(
+//        (function(s){
+//            return s.sayHello();
+//        })(this),1000
+//    );
+
+    var mm = this;
+    setTimeout(function(){
+        mm.sayHello();
+    },1000);
 };
 var f = new foo;
 f.bar();
